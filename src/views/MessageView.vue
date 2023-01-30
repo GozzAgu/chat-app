34e26638293b36a638bcf8cc77d60fd2448a3661
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2023-01-30 12:04:32 +0100

    fix: refactored messageView

--- src/views/MessageView.vue
+++ src/views/MessageView.vue
@@ -2,29 +2,28 @@
     <div id="messages" class="ml-[420px] w-full">
         <div class="w-full">
             <div class="border-l border-green-500 w-full">
-                <div class="fixed z-10 bg-[#F0F0F0] min- w-[calcl(100vw-420px)] flex justify-between items-center py-2 px-2">
+                <div class="fixed z-10 bg-[#F0F0F0] min- w-[calc(100vw-420px)] flex justify-between items-center py-2 px-2">
                     <div class="flex items-center">
                         <img src="../assets/dell-SGY0LIfTKZ4-unsplash.jpg" class="rounded-full mx-1 w-10" />
                         <div class="font-semibold ml-1 text-gray-900">
-                            Goziee
+                            Gozie
                         </div>
                     </div>
-                    
                     <DotsVerticalIcon fillColor="#515151"/>
                 </div>
             </div>
 
-            <div id="MessagesSection" class="pt-20 pb-8 z-[-1] h-[calc(100vh-65px)] w-[calc(100vh-420px)] overflow-auto fixed touch-auto">
+            <div id="MessagesSection" class="pt-20 pb-8 z-[-1] h-[calc(100vh-65px)] w-[calc(100vw-420px)] overflow-auto fixed touch-auto">
                 <div class="px-20 text-sm">
                     <div class="flex w-[calc(100%-50px)]">
-                        <div class="inline-block p-2 bg-white rounded-md my-1">
-                            Hi, I am Chief Hi, I am Chief Hi, I am Chief Hi, I am Chief Hi, I am Chief
+                        <div class="inline-block p-2 bg-gray-1   00 rounded-md my-1">
+                            Hi, I am Chief Hi, I am Chief Hi, I am Chief Hi, I am Chief Hi, I am Chief, I am Chief Hi, I am Chief Hi, I am Chief, I am Chief Hi, I am Chief Hi, I am Chief
                         </div>
                     </div>
 
                     <div class="float-right justify-end space-x-1 w-[calc(100%-50px)]">
-                        <div class="inline-block p-2 bg-white rounded-md my-1">
-                            Hi, I am Chief Hi, I am Chief Hi, I am Chief Hi, I am Chief Hi, I am Chief
+                        <div class="inline-block p-2 bg-green-200 rounded-md my-1">
+                            Hi, I am Chief Hi, I am Chief Hi, I am Chief Hi, I am Chief Hi, I am Chief, I am Chief Hi, I am Chief Hi, I am Chief, I am Chief Hi, I am Chief Hi, I am Chief
                         </div>
                     </div>
                 </div>
@@ -63,3 +62,7 @@ import EmoticonExcitedIcon from 'vue-material-design-icons/Emoticon.vue'
 import PaperclipIcon from 'vue-material-design-icons/Paperclip.vue'
 import SendIcon from 'vue-material-design-icons/Send.vue'
 </script>
+
+<style scoped>
+
+</style>
