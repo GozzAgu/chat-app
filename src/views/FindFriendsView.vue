e81a503afd8177117909f51a231a90f3f21feb84
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2023-01-30 12:04:12 +0100

    feat: created find friends component

---
+++ src/views/FindFriendsView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="FinfFriends" class="pt-[100px] overflow-auto fixed h-[100vh] w-full">
+        <div class="flex w-full p-4 items-center cursor-pointer">
+        <img src="../assets/dell-SGY0LIfTKZ4-unsplash.jpg" class="rounded-full mr-4 w-12" />
+
+        <div class="w-full">
+            <div class="flex justify-between items-center">
+            <div class="text-[15px] text-gray-600">
+                Gozie
+            </div>
+            </div>
+
+            <div class="flex items-center">
+            <div class="text-[15px] text-gray-500">
+                Hi, chat with me
+            </div>
+            </div>
+        </div>
+
+        
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
